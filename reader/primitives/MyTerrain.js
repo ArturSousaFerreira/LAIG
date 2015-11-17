8d085f43b939c09b54367d9e3cafa2b16eb99848
Rui <ruidcscg@sapo.pt>
2015-11-17 00:28:31 +0000

    MyTerrain alterado

--- reader/primitives/MyTerrain.js
+++ reader/primitives/MyTerrain.js
@@ -1,39 +1,50 @@
 function MyTerrain(scene, texture, heightmap) {
-    
     this.scene = scene;
-    CGFobject.call(this, this.scene);
+    CGFobject.call(this, scene);
 
     this.texture = texture;
     this.heightmap = heightmap;
 
-    this.materialTerrain = new CGFappearance(this.scene);
-    this.materialTerrain.setAmbient(0.5,0.5,0.5,1);
-    this.materialTerrain.setDiffuse(0.5,0.5,0.5,1);
-    this.materialTerrain.setSpecular(0.5,0.5,0.5,1);
-    this.materialTerrain.setShininess(1);
-    this.textureTerrain = new CGFtexture(this.scene, this.texture);
-    this.heightmapTerrain = new CGFtexture(this.scene, this.heightmap);
-    this.materialTerrain.setTexture(this.textureTerrain);
+    this.appearance = new CGFappearance(this.scene);
+	this.appearance.setAmbient(0.3, 0.3, 0.3, 1);
+	this.appearance.setDiffuse(0.7, 0.7, 0.7, 1);
+	this.appearance.setSpecular(0.0, 0.0, 0.0, 1);	
+	this.appearance.setShininess(120);
 
-    this.myShader = new CGFshader(this.scene.gl, "scenes/shaders/myShader.vert", "scenes/shaders/myShader.frag");
-    this.myShader.setUniformsValues({uSampler2: 1});
-    this.myShader.setUniformsValues({scale: 0.5});
+    this.texture_terrain = new CGFtexture(this.scene, this.texture);
+    this.heightmap_terrain = new CGFtexture(this.scene, this.heightmap);
+    this.appearance.setTexture(this.texture_terrain);
+    this.appearance.setTextureWrap ('REPEAT', 'REPEAT');
+
+    this.testShaders=[
+		new CGFshader(this.scene.gl, "scenes/shaders/flat.vert", "scenes/shaders/flat.frag"),
+		new CGFshader(this.scene.gl, "scenes/shaders/myShader.vert", "scenes/shaders/myShader.frag"),
+		new CGFshader(this.scene.gl, "scenes/shaders/uScale.vert", "scenes/shaders/uScale.frag"),
+		new CGFshader(this.scene.gl, "scenes/shaders/varying.vert", "scenes/shaders/varying.frag"),
+		new CGFshader(this.scene.gl, "scenes/shaders/texture1.vert", "scenes/shaders/texture1.frag"),
+		new CGFshader(this.scene.gl, "scenes/shaders/texture2.vert", "scenes/shaders/texture2.frag"),
+		new CGFshader(this.scene.gl, "scenes/shaders/texture3.vert", "scenes/shaders/texture3.frag")
+	];
+
+    this.testShaders[1].setUniformsValues({uSampler2: 1});
+    this.testShaders[1].setUniformsValues({scale: 0.5});
     
-    this.plane = new MyPlane(this.scene, 200);
+    this.plane = new MyPlane(this.scene, 250);
  }
  
 MyTerrain.prototype = Object.create(CGFobject.prototype);
 MyTerrain.prototype.constructor = MyTerrain;
  
 MyTerrain.prototype.display = function() {
-    this.materialTerrain.apply();
-    this.scene.setActiveShader(this.myShader);
+    this.appearance.apply();
+    this.scene.setActiveShader(this.testShaders[1]);
 
     this.scene.pushMatrix();
     
-    this.heightmapTerrain.bind(1);
+    this.heightmap_terrain.bind(1);
+    
     this.plane.display();
-    this.scene.setActiveShader(this.scene.defaultShader);
-
     this.scene.popMatrix();
+
+    this.scene.setActiveShader(this.scene.defaultShader);    
 };
